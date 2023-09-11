1821cef13f96dc22f02582bb265408d6d3e3688c
AliAhmedML <141727606+AliAhmedML@users.noreply.github.com>
2023-09-11 10:01:02 +0300

    Update Dog_vs_cat.py

--- Dog_vs_cat.py
+++ Dog_vs_cat.py
@@ -1,13 +1,15 @@
 # Importing the libraries
 import numpy as np
-import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 import cv2
-
+from tensorflow.keras.models import load_model
+import tensorflow as tf
+import tensorflow_hub as hub
+import requests
 # loading saved models
 
-Prediction_model = pickle.load(open('Dog_VS_Cat.sav', 'rb'))
+Prediction_model = load_model('C:/Users/ali ahmed/Desktop/Dog vs Cat Predicition system/catVsdog.keras', custom_objects={'KerasLayer':hub.KerasLayer})
 
 # Creating the sidebars
 with st.sidebar:
@@ -17,34 +19,40 @@ with st.sidebar:
     
 # Cat VS Dogs Prediction Page
 if (selected == 'Dog Vs Cat prediction'):
-    
-    # page title
-    st.title('Dog Vs Cat prediction using ML')
-    
-    image_url = st.text_input('Enter the image path')
-    
-    input_image = cv2.imread(image_url)
-
-    cv2.imshow(input_image)
-
-    input_image_resize = cv2.resize(input_image, (224,224))
-
-    input_image_scaled = input_image_resize/255
-
-    image_reshaped = np.reshape(input_image_scaled, [1,224,224,3])
-
-    prediction_result = ''
-    
-    if st.button('prediction Result'):
+    try :
+            # page title
+            st.title('Dog Vs Cat prediction using ML')
+            
+            image_url = st.text_input('Enter the image path')
+            
+            data = requests.get(image_url).content
+            
+            file = open('C:/Users/ali ahmed/Desktop/Dowloaded images/img.png','wb')
+            file.write(data)
+            file.close() 
+            
+            input_image = cv2.imread('C:/Users/ali ahmed/Desktop/Dowloaded images/img.png')
         
-        input_prediction = Prediction_model.predict(image_reshaped)
+            #cv2.imshow('Your image',input_image)
         
-        input_pred_label = np.argmax(input_prediction)
+            input_image_resize = cv2.resize(input_image, (224,224))
         
-        if input_pred_label == 0:
-            prediction_result = 'The image represents a Cat'
-
-        else:
-            prediction_result = 'The image represents a Dog'
-
-    st.success(prediction_result)
+            input_image_scaled = input_image_resize/255
+        
+            image_reshaped = np.reshape(input_image_scaled, [1,224,224,3])
+        
+            prediction_result = ''
+            
+            input_prediction = Prediction_model.predict(image_reshaped)
+                
+            input_pred_label = np.argmax(input_prediction)
+                
+            if input_pred_label == 0:
+                prediction_result = 'The image represents a Cat'
+        
+            else:
+                prediction_result = 'The image represents a Dog'
+        
+            st.success(prediction_result)
+    except ValueError:
+         pass
